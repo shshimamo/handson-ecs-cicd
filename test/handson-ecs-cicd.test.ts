55f59cbe055125d012842c1f8dd0dd41670143b3
hiroya.shimamoto <shshimamo@gmail.com>
2022-12-03 12:04:11 +0900

    cdk init

---
+++ test/handson-ecs-cicd.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HandsonEcsCicd from '../lib/handson-ecs-cicd-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/handson-ecs-cicd-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HandsonEcsCicd.HandsonEcsCicdStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
