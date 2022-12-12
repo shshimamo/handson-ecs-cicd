6cca9748b82b7dca0c08c8aca9a8573892cd3200
hiroya.shimamoto <shshimamo@gmail.com>
2022-12-13 02:52:49 +0900

    delete comment out

--- bin/handson-ecs-cicd.ts
+++ bin/handson-ecs-cicd.ts
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-// import { HandsonEcsCicdStack } from '../lib/handson-ecs-cicd-stack';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
 import { FrontendServiceStack } from '../lib/frontend-service-stack';
 import { Context } from '../lib/common/context'
 
 const app = new cdk.App();
-// new HandsonEcsCicdStack(app, 'HandsonEcsCicdStack', {
-//     env: {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION},
-// });
 
 const infra = new InfrastructureStack(app, `${Context.ID_PREFIX}-VpcStack`, {
     env: {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION},
